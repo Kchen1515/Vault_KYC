d7a4831874d477dadc0904e227defc8de5cc5564
Kchen1515 <99617496+Kchen1515@users.noreply.github.com>
2022-06-16 18:39:18 -0400

    finished herobanner commit

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { Product, FooterBanner, HeroBanner } from '../components'
 import { client } from '../lib/client'
 
-const Home = () => {
+const Home = ({products, bannerData}) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations </p>
       </div>
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
@@ -29,7 +30,7 @@ export const getServerSideProps = async () => {
   return {
     props: {products, bannerData}
   }
-  
+
 }
 
 export default Home
