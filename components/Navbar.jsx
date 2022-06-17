4a61cadabe8b81294f6eb48a1e1bf5fcf4a49f8c
Kchen1515 <99617496+Kchen1515@users.noreply.github.com>
2022-06-17 17:37:10 -0400

    final commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href='/'> Rythmn </Link>
+        <Link href='/'> VAULT - KYC </Link>
       </p>
       <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping/>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Product, FooterBanner, HeroBanner } from '../components'
+import { Product,  HeroBanner } from '../components'
 import { client } from '../lib/client'
 
 const Home = ({products, bannerData}) => (
@@ -15,7 +15,7 @@ const Home = ({products, bannerData}) => (
         {products?.map((product) => <Product key={product._id} product={product}/>)}
       </div>
 
-      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+      {/* <FooterBanner footerBanner={bannerData && bannerData[0]} /> */}
     </div>
   
 )
