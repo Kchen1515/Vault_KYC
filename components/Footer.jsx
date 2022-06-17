1c275ee5798a57bbced2f57535582abd52d4e9cd
Kchen1515 <99617496+Kchen1515@users.noreply.github.com>
2022-06-16 20:49:33 -0400

    Additional Page commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2022 KYC All Rights Reserved</p>
+      <p className='icons'>
+        <a href='https//www.instagram.com'><AiFillInstagram/></a>
+        <a href='https//www.instagram.com'><AiOutlineTwitter/></a>
+        
+      </p>
+    </div>
   )
 }
 
